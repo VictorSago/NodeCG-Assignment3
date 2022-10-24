33589ba044e54f0952e14641d733607bb4bda2b3
Victor Sago <victor.sago@elstandard.se>
2022-10-24 14:49:45 +0200

    Static css files served with the pages

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-//const path = require("path");
+const path = require("path");
 const express = require("express");
+
+
+const rootDir = require("./util/pathhelper");
+const homeRoutes = require("./routes/index");
 
 const app = express();
 
-//const rootDir = require("./util/pathhelper");
-const homeRoutes = require("./routes/index");
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/", homeRoutes);
-
 
 app.listen(3000);
